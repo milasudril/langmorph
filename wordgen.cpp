3970f4ec603e4db1d9b4ea432a481731dc7a9999
Torbjörn Rathsman <milasudril@gmail.com>
2021-12-20 19:49:31 +0100

    List vowels and consonants

--- wordgen.cpp
+++ wordgen.cpp
@@ -9,7 +9,12 @@ using CodeSequence = std::string;
 
 auto single_wovels()
 {
-	return std::set<CodeSequence>{"a", "e", "i", "o", "u", "y", "ä", "å", "ö"};
+	return std::set<CodeSequence>{"a", "e", "i", "o", "u", "y", "ä", "å", "é", "ö", "ü"};
+}
+
+auto single_consonants()
+{
+	return std::set<CodeSequence>{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "þ" ,"ð"};
 }
 
 int main()
