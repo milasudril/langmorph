18aa2c7a3b83c405c13c7b62c0788fa1913aeec5
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-15 12:04:39 +0100

    Add functionality to io_utils

--- io_utils.hpp
+++ io_utils.hpp
@@ -6,6 +6,7 @@
 #include <filesystem>
 #include <cerrno>
 #include <cstring>
+#include <ranges>
 
 namespace langmorph
 {
@@ -28,8 +29,8 @@ namespace langmorph
 		return std::pair{file_handle{fopen(filename, std::forward<Args>(args)...)}, errno};
 	}
 
-	template<class T, class ... Args>
-	auto load(fs::path&& filename, T&& loader, Args... args)
+	template<class Loader, class ... Args>
+	auto load(fs::path const& filename, Loader&& loader, Args&&... args)
 	{
 		auto res = langmorph::create_file(filename.c_str(), "rb");
 		if(res.first == nullptr)
@@ -44,6 +45,34 @@ namespace langmorph
 		return loader(res.first.get(), std::forward<Args>(args)...);
 	}
 
+	template<class T>
+	concept file_list = std::ranges::input_range<T> && requires(T a)
+	{
+		{a->c_str()} -> std::same_as<char const*>;
+	};
+
+	template<file_list FileList, class Loader, class ... Args>
+	auto load(FileList&& file_list, Loader&& loader, Args&&... args)
+	{
+		auto i = std::begin(file_list);
+		auto last_error = errno;
+		while(i != std::end(file_list))
+		{
+			auto f = create_file(i->c_str(), "rb");
+			if(f.first != nullptr)
+			{
+				return loader(f.first.get(), std::forward<Args>(args)...);
+			}
+			last_error = f.second;
+			++i;
+		}
+
+		auto errmsg = strerror(last_error);
+		throw std::runtime_error{std::string{"Failed to open "}.append(to_string(file_list))
+			.append(":")
+			.append(errmsg)
+		};
+	}
 }
 
 #endif
